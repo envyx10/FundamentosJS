2f7baad9484375d07eb0b89b4f2cd3a6ab86be94
Envy <pabloinfodesign95@gmail.com>
2025-06-27 14:05:29 +0200

    Refactor la función saludar y reorganiza el código para mejorar la legibilidad
    retorno de funciones del curso

--- JAVASCRIPT/6 - FuncionesBasicas/funciones.js
+++ JAVASCRIPT/6 - FuncionesBasicas/funciones.js
@@ -1,8 +1,15 @@
 
 // Funcion basica
 function saludar(nombre) {
-    console.log(arguments);
-    console.log(`Hola ${nombre}`);
+
+    // console.log(arguments);
+    // console.log(`Hola ${nombre}`);
+
+    return [1,2,3,4,5];
+
+    // cualquier cosa que esta debajo de un return no se ejecuta
+    console.log('Soy un código que está después del return');
+    
 }
 
 // Funcion anonima
@@ -16,6 +23,31 @@ const saludarFecha = (nombre) => {
     console.log(`Hola ${nombre}`);
 }
 
-saludar('Pablo', 30, true , 'Málaga')
-saludar2('Elle')
-saludarFecha('Flecha')
+const retornoDesaludar = saludar ('Pablo', 30, true , 'Málaga') // 1
+console.log(retornoDesaludar);
+
+// saludar2('Elle')
+// saludarFecha('Flecha')
+
+function sumar(a,b){
+    return a + b
+}
+
+console.log(sumar(1,2));
+
+// Si solo tenemos una linea y es return podemos hacerlo de esta manera tambien
+const sumar2 = (a,b) => a + b
+
+console.log(sumar2(2,2));
+
+// Devolvemos mediante Math una funcion que de manera random te da un numero 
+function getRandom(){
+    return Math.random();
+}
+
+console.log(getRandom());
+
+// Misma forma con function arrow y al ser 1 unico return, se omite
+const getRandom2 = () => Math.random()
+
+console.log( getRandom2() );
