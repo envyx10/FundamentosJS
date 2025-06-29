6b65991c50ab54f0e36cbd4464e64b2a53535e30
Envy <pabloinfodesign95@gmail.com>
2025-06-29 21:50:59 +0200

    Eliminar archivo protip-return.js y actualizar index.html para incluir bools.js

---
+++ JAVASCRIPT/10 - LogicaBooleana/bools.js
@@ -0,0 +1,47 @@
+
+
+
+const regresaTrue = () => {
+    console.log('Regresa true');
+    return true
+}
+
+const regresaFalse = () => {
+    console.log('Regresa false');
+    return false
+}
+
+console.warn('Not o la negacion')
+    console.log(true);   // true
+    console.log(!true);  // false
+    console.log(!false); // true
+
+console.log(!regresaFalse());
+console.log(!regresaTrue());
+
+console.warn('And') // En and && si hay 1 false aunque haya 20 trues, siemopre será false
+    console.log( true & true);  // true
+    console.log( false & true); // false
+    console.log( true & false); // false
+
+console.log( !true & false); // false
+console.log( !false & !false); // true
+console.log( !true & false); // false
+
+console.log('==========='); 
+
+// Operador and && si regresa falsa, ignora cualquier cosa que venga despúes 
+console.log( regresaFalse() && regresaTrue()); // false
+
+// En este caso al empezar por true continua 
+console.log( regresaTrue() && regresaFalse()); // false
+
+console.warn('OR'); // Lo opuesto a &&
+    console.log( true || false );
+    console.log( false || false );
+
+    console.log(regresaTrue() || regresaFalse());
+    
+
+console.warn('Asignaciones');
+    
