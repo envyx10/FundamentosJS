8a1100071e61e2c02bba1169a09a5fb5f877c90a
Envy <pabloinfodesign95@gmail.com>
2025-06-29 21:33:58 +0200

    Agregar ejercicios utilizando objetos y arrays para manejar días de la semana

--- JAVASCRIPT/9 - If-else/if-else.js
+++ JAVASCRIPT/9 - If-else/if-else.js
@@ -28,3 +28,56 @@ if (dia === 0) {
 } else if ( dia === 1){
     console.log('Es lunes');
 }
+
+
+// Ejercicio sin usar if else o switch, únicamente objetos o arrays para realizar la misma tarea 
+// e imprimir la tarea 
+
+const diasLetras = { // Mi forma
+  domingo: 0,
+  lunes: 1, 
+  martes: 2, 
+  miercoles: 3, 
+  jueves: 4, 
+  viernes : 5, 
+  sabado: 6, 
+} 
+
+console.log(diasLetras.miercoles);
+
+// Con array
+diararo = 5
+const diasLetrasArray = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sábado']
+console.log(diasLetrasArray[diararo]);
+
+// Resultado de devtalles
+const diasLetras2 = { 
+  0: 'domingo',
+  1: 'lunes', 
+  2: 'martes', 
+  3: 'miercoles', 
+  4: 'jueves', 
+  5: 'viernes', 
+  6: 'sabado', 
+}
+
+dia1 = 3
+
+console.log(diasLetras2[dia1] || 'Dia no definido');
+
+// Resultado de talles forma 2
+const diasLetras3 = { 
+  0: ()=> 'domingo - 0',
+  1: ()=> 'lunes - 1', 
+  2: ()=> 'martes - 2', 
+  3: ()=> 'miercoles - 3', 
+  4: ()=> 'jueves - 4', 
+  5: ()=> 'viernes - 5', 
+  6: ()=> 'sabado - 6', 
+}
+
+dia2= 3
+console.log(diasLetras3[dia2]() || 'Dia no definido');
+
+
+
