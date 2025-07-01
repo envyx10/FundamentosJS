6f3db3275eb7346edb0091ebbe0ed4e64de63726
Envy <pabloinfodesign95@gmail.com>
2025-07-01 22:14:29 +0200

    Agregar documentación y mejorar la función crearDeck para incluir la extracción de cartas del mazo

--- JAVASCRIPT/02-Laboratorio/Blackjack/assets/js/juego.js
+++ JAVASCRIPT/02-Laboratorio/Blackjack/assets/js/juego.js
@@ -15,6 +15,10 @@ let deck         = []
 const tipos      = [ 'C','D','H','S']
 const especiales = [ 'A','J','Q','K']
 
+/**
+ * Esta funcion crea una nueva baraja
+ * @returns 
+ */
 const crearDeck = () => {
 
     for ( let i = 2 ; i <= 10  ; i++ ){
@@ -32,10 +36,53 @@ const crearDeck = () => {
 
     }
 
-    console.log(deck); 
+  
     deck = _.shuffle(deck)
     console.log(deck); 
+
     return deck
 }
 
 crearDeck()
+
+
+/**
+ * Funcion que permite tomar una carta
+ * @returns 
+ * 
+ * Tarea: crear const carta // extraer del deck una carta de manera aleatoria
+ * 
+ */
+const pedirCarta = () => {
+
+    if ( deck.length === 0){
+        throw 'No hay cartas en el Deck'
+    }
+
+    const carta = deck.pop();
+
+    console.log(deck);
+
+    return carta
+
+}
+
+pedirCarta()  
+
+
+/**
+ * Valor de cada carta
+ */
+const valorCarta = ( carta ) => {
+
+    //  substring() método devuelve un subconjunto de un objeto String.
+    const valor = carta.substring( 0 , carta.length -1 ) 
+
+    return ( isNaN( valor ) ) ? ( valor === 'A' ) ? 11 : 10 
+                                                       : valor * 1         
+     
+
+}
+
+const valor = valorCarta('5D')
+console.log(valor);
