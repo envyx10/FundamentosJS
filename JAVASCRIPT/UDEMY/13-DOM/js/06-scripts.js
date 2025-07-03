973a3ca580abb287dafe378740dbe2454cea4b38
Envy <pabloinfodesign95@gmail.com>
2025-07-03 23:45:45 +0200

    Actualizar index.html y scripts de JavaScript: corregir el ID del formulario en index.html y renombrar archivos de scripts para una mejor organización.

--- JAVASCRIPT/UDEMY/13-DOM/js/06-scripts.js
+++ JAVASCRIPT/UDEMY/13-DOM/js/06-scripts.js
@@ -1,44 +1,38 @@
-// Seleccionar elementos del DOM nos va a permitir modificar nuestro HTML y hacerlo más interactivo..
+// Seleccionar elementos del DOM nos va a permitir modificar 
+// nuestro HTML y hacerlo más interactivo..
 
 // Veamos por ejemplo como modificar el texto de la parte superior..
 
-// Primero tenemos que seleccionarlo...
 
-const encabezado = document.querySelector('.contenido-hero h1');
-// console.log(encabezado);
+const encabezado = document.querySelector('.contenido-hero h1')
+console.log(encabezado);
 
-// Si deseas acceder al texto hay 3 formas de hacerlo...
-console.log(encabezado.innerText); 
-console.log(encabezado.textContent);
-console.log(encabezado.innerHTML);
+// Acceder al texto mediante JS
 
-// Cual es la diferencia entre estas 3... bueno, si le ponemos a ese elemento visibility: hidden; en el CSS, 
+// InnerText no encuentra un texto oculto con visibility hidden
+console.warn('InnerText');
+    console.log(encabezado.innerText);
 
-// Puedes ver que innerText no puede acceder a ese contenido...
+// Pero si lo va a encontrar con textContent
+console.warn('textContent');
+    console.log(encabezado.textContent);
 
-// Ahora, si añadimos algo de HTML, 
+// Se trae el texto y html entero
+console.warn('innerHTML');
+    console.log(encabezado.innerHTML);
 
-// TextContent va a ignorar esas etiquetas HTML y traer solo el texto, mientras que innerhTML si se trae el HTML..
+console.warn('Misma linea');
+// tambien puedes hacerlo en la misma linea
+    const encabezado1 = document.querySelector('.contenido-hero h1').innerHTML
+        console.log(encabezado1);
 
-// ahora también puedes tener algo de encadenamiento o chaining...
-const textoEncabezado = document.querySelector('.contenido-hero h1').textContent;
-console.log(textoEncabezado);
+// Esto modifica el DOM y cambia nuestro h1 con el texto ' nuevo heading '
+const encabezado2 = document.querySelector('.contenido-hero h1').innerHTML = 'nuevo heading'
+        console.log(encabezado2);
 
-// Ahora, aquí puedes ver que estoy asignando el Texto a una variable, pero también puedes modificarlo de diferentes formas...
+// Esto modifica una imagen
+const imagen = document.querySelector('.card img')
+    console.log(imagen);
+    
+imagen.src = 'img/hacer3.jpg'
 
-document.querySelector('.contenido-hero h1').textContent = 'Nuevo Heading'
-
-// otra opción seria con una variable.
-
-const nuevoTexto = 'Nuevo Heading';
-document.querySelector('.contenido-hero h1').textContent = nuevoTexto;
-
-
-// También puedes cambiar una imagen...
-
-// No lo hemos visto, pero veamos por ejemplo como acceder a esa imagen...
-
-const imagen = document.querySelector('.card img');
-console.log(imagen.src);
-// Cambiar la imagen...
-imagen.src = 'img/hacer2.jpg';
