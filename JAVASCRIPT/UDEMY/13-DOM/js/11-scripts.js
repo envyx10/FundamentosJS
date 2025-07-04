75914caf62245f80201baa64f15bdae79d0de0cd
Envy <pabloinfodesign95@gmail.com>
2025-07-04 13:20:16 +0200

    Actualizar index.html para referenciar correctamente 11-scripts.js y mejorar la organización del código en 11-scripts.js.

--- JAVASCRIPT/UDEMY/13-DOM/js/11-scripts.js
+++ JAVASCRIPT/UDEMY/13-DOM/js/11-scripts.js
@@ -1,20 +1,21 @@
-// No siempre estarás haciendo traversing a tu dom, 
+// No siempre estarás haciendo traversing a tu dom
+const btnFlotante = document.querySelector(".btn-flotante");
+const footer = document.querySelector(".footer");
 
-const btnFlotante = document.querySelector('.btn-flotante');
+// Eventos
 
-
-const footer = document.querySelector('.footer');
-
-btnFlotante.addEventListener('click', mostrarOcultarFooter);
-
+// Esto es como decir '' estate atento por si alguien hace click,
+// puede que tengas que hacer algo ''
 function mostrarOcultarFooter() {
-    if( footer.classList.contains('activo') ) {
-        footer.classList.remove('activo');
-        this.classList.remove('activo');
-        this.textContent = 'Idioma y Moneda';
+    if (footer.classList.contains("activo")) {
+        footer.classList.remove("activo");
+        this.classList.remove('activo')
+        this.textContent = 'Idioma y moneda'
     } else {
-        footer.classList.add('activo');
-        this.classList.add('activo');
-        this.textContent = 'X Cerrar';
+        footer.classList.add("activo");
+        this.classList.add('activo')
+        this.textContent = 'X cerrar'
     }
 }
+
+btnFlotante.addEventListener("click", mostrarOcultarFooter);
