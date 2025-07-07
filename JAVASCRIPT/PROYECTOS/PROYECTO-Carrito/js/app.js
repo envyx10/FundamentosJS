6d789351a47b6c29b8f5f03602e72e2b66fa95d0
Envy <pabloinfodesign95@gmail.com>
2025-07-07 15:33:52 +0200

    Refactorizar función agregarCurso: mover la lógica de selección de curso a una función separada

--- JAVASCRIPT/PROYECTOS/PROYECTO-Carrito/js/app.js
+++ JAVASCRIPT/PROYECTOS/PROYECTO-Carrito/js/app.js
@@ -19,9 +19,33 @@ function agregarCurso(e){
 
     // Basicamente si la clase contiene agregar-carrito, mostramos el log '' agregando al carrito ''
     if(e.target.classList.contains('agregar-carrito')){
-        console.log(e.target);
+        const cursoSeleccionado = e.target.parentElement.parentElement
+        leerDatosCurso(cursoSeleccionado)
     }
     
 }
+
+
+// Lee el contenido del HTML al que le dimos click y extrae la info del curso
+function leerDatosCurso(curso){
+    
+    console.log(curso);
+
+    // Objeto con el contenido del curso actual
+    const infoCurso = {
+        // En este caso agregamos curso. y no document, xq curso ya tiene el contexto del div entero
+        imagen: curso.querySelector('img').src, // extraemos contenido del div - img
+        titulo: curso.querySelector('h4').textContent, // extraemos contenido del div - titulo
+        nombre: curso.querySelector('p').textContent, // extraemos contenido del div - nombre
+        precio: curso.querySelector('.precio span').textContent, // extraemos contenido del div - precio
+        id:     curso.querySelector('a').getAttribute('data-id'), // extraemos contenido del div - id
+        cantidad: 1
+        
+    }
+
+    console.log(infoCurso);
+    
+}
+
 
 cargarEventListeners() ;
