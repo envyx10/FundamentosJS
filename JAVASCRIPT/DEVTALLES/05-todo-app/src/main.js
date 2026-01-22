453bc80cb4855e34288dac8d9461a93684073f25
Envy <pabloinfodesign95@gmail.com>
2026-01-22 12:56:59 +0100

    feat: initialize todo app with Vite setup and basic structure
    - Added package.json for Vite configuration
    - Included SVG icons for Vite and JavaScript
    - Created main.js to bootstrap the application
    - Added style.css for basic styling of the todo app
    - Implemented app.html for the todo app structure
    - Developed app.js to manage the todo app functionality

---
+++ JAVASCRIPT/DEVTALLES/05-todo-app/src/main.js
@@ -0,0 +1,10 @@
+import './style.css'
+import  { App } from './todos/app.js'
+
+console.log('Hola mundo');
+
+/* 
+  Este modulo es el Padre 
+  Es quien le dice al Hijo lo que debe hacer
+*/
+App('#app');
---
+++ JAVASCRIPT/DEVTALLES/05-todo-app/src/todos/app.js
@@ -0,0 +1,36 @@
+
+// ?raw es algo especifico de vite
+// Le dice, no intentes ejecturar el html, 
+// simplemente copialo tal cual y pasalo a la variable html
+import html from './app.html?raw';
+
+/**
+ * 
+ * @param {String} elementId 
+ */
+export const App = ( elementId ) => {
+
+    // Funcion anonima autoinvocada (IIFE)
+
+    (() => {
+
+        // Traducción: Crea una caja vacía de cartón (un <div>) en la memoria de JavaScript.
+        // Ojo: Esta caja todavía está "flotando" en el aire (en memoria), 
+        // no está puesta en la página web visible todavía.
+        const app = document.createElement('div');
+
+        // Aqui le decimos que meta en el div vacio creado con createElement
+        // la variable html donde se encuentra copiado todo el html raw de la importación de arriba
+        app.innerHTML = html ;
+
+        //document.querySelector(elementId): 
+        // Busca en la página web real el elemento con el ID que te pasaron (ej: #app). Encuentra el terreno.
+
+        //.append( app ): 
+        // Agarra la caja de cartón que preparamos (la variable app) y incrustala físicamente en ese terreno.
+        document.querySelector(elementId).append( app );
+
+    })();
+
+
+}
