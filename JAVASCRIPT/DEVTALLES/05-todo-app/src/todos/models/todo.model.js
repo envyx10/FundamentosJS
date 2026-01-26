599ae88bbfcfdca58fe5a7fc2b7768db1742052c
Envy <pabloinfodesign95@gmail.com>
2026-01-26 11:51:49 +0100

    feat: utilizar uuid para generación de identificadores únicos en tareas

--- JAVASCRIPT/DEVTALLES/05-todo-app/src/todos/models/todo.model.js
+++ JAVASCRIPT/DEVTALLES/05-todo-app/src/todos/models/todo.model.js
@@ -1,4 +1,4 @@
-
+import { v4 as uuid } from "uuid"; // npm i uuid - paquete para generar ids que nunca se repitan
 
 export class Todo {
 
@@ -8,7 +8,7 @@ export class Todo {
      */
     constructor( descripcion ){
 
-        this.id = 1;
+        this.id = uuid(); // Con el paquete uuid hacemos que el id jamás se repita ya que matemáticamente esta creado para que sea imposible.
         this.description = descripcion
         this.done = false;
         this.createdAt = new Date();
